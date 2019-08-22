efae049bbe35d244d4b6e4fce03ab17fcedfa1d6
Carlos Molano <926@holbertonschool.com>
2019-08-22 15:27:30 -0500

    Adding feature: skip crtl+c

--- 8-super_simple_shell.c
+++ 8-super_simple_shell.c
@@ -1,4 +1,24 @@
 #include "header.h"
+#include <signal.h>
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define RESET "\x1B[0m"
+
+/**
+ * handle_sign - ignore the ctrl+2 and re-start the shell process.
+ * @sig: number of signal.
+ * Return: Always success.
+ */
+void handle_sign(int sig)
+{
+	char *prompt;
+	(void) sig;
+
+	prompt = GRN "mini-shell--" RESET RED "$ " RESET;
+	signal(SIGINT, handle_sign);
+	write(STDIN_FILENO, "\n", 1);
+	write(STDIN_FILENO, prompt, _strlen(prompt));
+}
 /**
  * main - super simple shell.
  * @argc: arguments counter.
@@ -6,6 +26,7 @@
  * @env: enviromental variables vector.
  * Return: Always 0.
  */
+
 int main(int argc, char **argv, char **env)
 {
 	char *line = NULL;
@@ -13,6 +34,8 @@ int main(int argc, char **argv, char **env)
 	int status = 1;
 	int chk_build = 0;
 	(void)argc;
+
+	signal(SIGINT, handle_sign);
 
 	while (status)
 	{
