cfeb30522968c8bd6fe5d142e2f966527c4b35ba
David Peralta <924@holbertonschool.com>
2019-08-27 14:15:50 -0500

    fix split_line.c betty and clean up backup

--- split_line.c
+++ split_line.c
@@ -6,39 +6,39 @@
  */
 char **split_line(char *line)
 {
-  char delim[] = " \n\t\r\a";
-  char *token;
-  char **av;
-  /*Remember coun_words to 0 for the real case*/
-  int count_words = 1, count_buff = 0, i, j;
+	char delim[] = " \n\t\r\a";
+	char *token;
+	char **av;
+	/*Remember coun_words to 0 for the real case*/
+	int count_words = 1, count_buff = 0, i, j;
 
-  for (i = 0; line[i]; i++)/*Count num of words to define malloc*/
-    {
-      for (j = 0; delim[j]; j++)
+	for (i = 0; line[i]; i++)/*Count num of words to define malloc*/
 	{
-	  if (line[i] == delim[j])
-	    count_words++;
+		for (j = 0; delim[j]; j++)
+		{
+			if (line[i] == delim[j])
+				count_words++;
+		}
 	}
-    }
-  /*Get the 1st token*/
-  token = _strtok(line, delim);
-  av = _calloc(count_words, sizeof(char *));
-  if (av == NULL)
-    return (NULL);
-  /*Get other tokens*/
-  while (token != NULL)
-    {
-      av[count_buff] = _strdup(token);
-      if (av[count_buff] == NULL)
+	/*Get the 1st token*/
+	token = _strtok(line, delim);
+	av = _calloc(count_words, sizeof(char *));
+	if (av == NULL)
+		return (NULL);
+	/*Get other tokens*/
+	while (token != NULL)
 	{
-	  _freearrp(av);
-	  return (NULL);
+		av[count_buff] = _strdup(token);
+		if (av[count_buff] == NULL)
+		{
+			_freearrp(av);
+			return (NULL);
+		}
+		count_buff++;
+		token = _strtok(NULL, delim);
 	}
-      count_buff++;
-      token = _strtok(NULL, delim);
-    }
-  /*Finish the **array with NULL*/
-  av[count_buff] = NULL;
+	/*Finish the **array with NULL*/
+	av[count_buff] = NULL;
 
-  return (av);
+	return (av);
 }
